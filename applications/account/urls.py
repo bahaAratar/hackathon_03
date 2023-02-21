069d33f78b26f7b608813e229fcdc09297035c57
Aratar <58032725+bahaAratar@users.noreply.github.com>
2023-02-21 22:30:55 +0600

    решение конфликта

--- applications/account/urls.py
+++ applications/account/urls.py
@@ -1,10 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from applications.account.views import *
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django_rest_passwordreset.views import reset_password_request_token
 
 urlpatterns = [
     path('register/', RegisterAPIView.as_view()),
     path('activate/<uuid:activation_code>/', ActivationView.as_view()),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('account/', MyUsertList.as_view()),
+    path('account/<int:pk>/', MyUserDetail.as_view()),
+    path('password_reset/', reset_password_request_token, name='password_reset'),
 ]
