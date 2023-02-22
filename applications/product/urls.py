9fb686ef4ccc75e8995e7480d9f46ef97b6a206f
Aratar <58032725+bahaAratar@users.noreply.github.com>
2023-02-22 17:40:40 +0600

    Update urls.py

--- applications/product/urls.py
+++ applications/product/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from applications.product.views import ProductModelViewSet, CreateImageAPIView
 from .views import *
 
 router = DefaultRouter()
@@ -8,4 +9,6 @@ router.register('categoryes', CategorytViewSet)
 
 urlpatterns = [
     path('', include(router.urls)), 
+    path('tag/<slug:tag_slug>/',ProductModelViewSet.product_list, name='product_list_by_tag'),
+    path('add/image/', CreateImageAPIView.as_view())
 ]
