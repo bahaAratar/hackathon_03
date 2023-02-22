8356597abc1e9924017c8e6b56e2b7edb8577356
Aratar <58032725+bahaAratar@users.noreply.github.com>
2023-02-22 16:47:33 +0600

    Update admin.py

--- applications/product/admin.py
+++ applications/product/admin.py
@@ -1,1 +1,19 @@
 from django.contrib import admin
+from applications.product.models import Product, ProductImage
+from django.contrib import admin
+from .models import Product
+
+class ImageAdmin(admin.TabularInline):
+    model = ProductImage
+    fields = ('image',)
+    max_num = 5
+
+
+@admin.register(Product)
+class PostAdmin(admin.ModelAdmin):
+    inlines = (ImageAdmin,)
+    list_display = ('name', 'slug', 'owner', 'price','tags')
+    list_filter = ('owner', )
+    search_fields = ('name', )
+    prepopulated_fields = {'slug': ('name',)}
+    raw_id_fields = ('owner',)
