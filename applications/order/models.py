ce9f9bb29776148505298f554d999535817dd404
Aratar <dead.baha.31@gmail.com>
2023-02-22 13:05:14 +0600

    новое исправление ошибок

--- applications/order/models.py
+++ applications/order/models.py
@@ -1,9 +1,10 @@
+from django.conf import settings
 from django.db import models
-from django.contrib.auth.models import User
-from ..product import Product
+from ..account.models import MyUser
+from applications.product.models import Product
 
 class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(MyUser, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product)
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateTimeField(auto_now_add=True)
