0cce99496633d8a2caddf42e4316b774a642aeb7
Aratar <58032725+bahaAratar@users.noreply.github.com>
2023-02-22 16:35:33 +0600

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -159,7 +159,7 @@ REST_FRAMEWORK = {
     ],
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination', 
     #     'rest_framework.pagination.LimitOffsetPagination',
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination'
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 5,
 }
 
