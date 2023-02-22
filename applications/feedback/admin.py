84823bf78998009f890c1d2e6aea15e11f1d6109
eRiKa4530 <omurbekovaerika@gmail.com>
2023-02-22 11:27:23 +0600

    hello

--- applications/feedback/admin.py
+++ applications/feedback/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Like
+admin.site.register(Like)
--- applications/feedback/models.py
+++ applications/feedback/models.py
@@ -1,5 +1,12 @@
 from django.db import models
+# from applications.account.models import MyUser
+# from applications.product.models import Product
 
-class Feedback(models.Model):
-    pass
+# class Like(models.Model):
+#     owner = models.ForeignKey(MyUser,on_delete=models.CASCADE,related_name='likes')
+#     post = models.ForeignKey(Product,on_delete=models.CASCADE,related_name='likes')
+#     is_like = models.BooleanField(default=False)
+#     def __str__(self) :
+#         return f'{self.owner} liked - {self.post.title}'
+
 
