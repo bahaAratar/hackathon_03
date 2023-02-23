d42f566c465b731b7cf9ed10a00ba81ad7200ea4
eRiKa4530 <omurbekovaerika@gmail.com>
2023-02-23 13:03:23 +0600

    key

--- applications/contact/views.py
+++ applications/contact/views.py
@@ -1,13 +1,19 @@
 
 
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from rest_framework import viewsets
-from .models import Contact
+from .models import Contact,Delivery
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from .serializers import ContactSerializer
+from .serializers import ContactSerializer, DeliverySerializer
+from rest_framework.decorators import action
+
 
 class ContactViewSet(viewsets.ModelViewSet): # post , get
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-
+class DeliveryViewSet(viewsets.ModelViewSet):
+    queryset = Delivery.objects.all()
+    serializer_class = DeliverySerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    
