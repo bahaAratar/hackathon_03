bde9f6bb036af4ca491aa44589fe44b64fd74a6a
Aratar <58032725+bahaAratar@users.noreply.github.com>
2023-02-23 16:15:46 +0600

    Update urls.py

--- applications/feedback/urls.py
+++ applications/feedback/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path, include
 from applications.feedback.views import *
 from rest_framework.routers import DefaultRouter
-from applications.product.views import *
 
     
 router = DefaultRouter()
 # router.register('comments', CommentViewSet, basename='comment')
 router.register('comment', CommentModelViewSet)
-
 router.register('rating', RatingModelViewSet)
 router.register('favorit', FavoriteModelViewSet)
+router.register('like', LikeModelViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
