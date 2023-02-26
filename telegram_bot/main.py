5b5d213e8462d38f6cf8508684b77264924eb56b
Aratar <58032725+bahaAratar@users.noreply.github.com>
2023-02-26 21:22:53 +0600

    Delete telegram_bot directory

--- telegram_bot/main.py
+++
@@ -1,5 +0,0 @@
-import telebot
-from decouple import config
-
-bot = telebot.TeleBot(config('TELEGRAM_TOKEN'))
-
--- telegram_bot/parser.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup as BS
-from decouple import config
-
-URL = config('SITE_URL')
-r = requests.get(URL)
-print(r.json())
-
